bb18fbe872db08d7511802833720e6b61d87b207
Maria Clara Freire Teles <mariaclarafreireteles@gmail.com>
2025-03-12 15:46:27 -0300

    config(views.py) :sparkles: Adicionada função get_by_nick e CRUD

--- api_rest/views.py
+++ api_rest/views.py
@@ -19,3 +19,33 @@ def get_users(request):
         return Response(serializer.data)
     
     return Response(status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET'])
+def get_by_nick(request, nick):
+    try:
+        user = User.objects.get(pk=nick)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+    
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def user_manager(request):
+    if request.method == 'GET':
+        try:
+            if request.GET['user']:
+                user_nickname = request.GET['user']
+
+                user = User.objects.get(pk=user_nickname)
+
+                serializer = UserSerializer(user)
+                return Response(serializer.data)
+            
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+                
